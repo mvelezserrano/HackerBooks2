ada1762b8a995fc559a537e5e3c65af96cbe5149
Miguel Ángel Vélez <mvelezserrano@outlook.es>
2015-04-15 11:44:43 +0200

    Initial Commit

---
+++ HackerBooks2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HackerBooks2
+//
+//  Created by Miguel Angel Vélez Serrano on 15/4/15.
+//  Copyright (c) 2015 Miguel Ángel Vélez Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ HackerBooks2Tests/HackerBooks2Tests.m
@@ -0,0 +1,40 @@
+//
+//  HackerBooks2Tests.m
+//  HackerBooks2Tests
+//
+//  Created by Miguel Angel Vélez Serrano on 15/4/15.
+//  Copyright (c) 2015 Miguel Ángel Vélez Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface HackerBooks2Tests : XCTestCase
+
+@end
+
+@implementation HackerBooks2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
